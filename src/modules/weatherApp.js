1c8be109d812c8079a38bd82c5311af4768f6997
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-08-26 17:49:28 +0100

    Add weekly forecast that iterates through each day and prints to console

--- src/modules/weatherApp.js
+++ src/modules/weatherApp.js
@@ -9,14 +9,27 @@ export default class WeatherApp {
   async showCurrentWeather(location) {
     const weatherData = await this.weatherAPI.getCurrentWeather(location);
     const currentWeather = new WeatherData(
-      weatherData.resolvedAddress,
+      weatherData.days[0].datetime,
       weatherData.days[0].conditions,
       weatherData.days[0].temp,
       weatherData.days[0].tempmin,
       weatherData.days[0].tempmax,
       weatherData.days[0].humidity,
-      weatherData.days[0].feelslike
+      weatherData.days[0].feelslike,
+      weatherData.resolvedAddress
     );
-    console.log(currentWeather.toString());
+    console.log(currentWeather.currentToString());
+  }
+
+  async showWeeklyForecast(location) {
+    const forecastData = await this.weatherAPI.getWeeklyForecast(location);
+    forecastData.days.slice(1).forEach(day => {
+      const dailyWeather = new WeatherData(
+        day.datetime,
+        day.conditions,
+        day.temp
+      );
+      console.log(dailyWeather.weeklyToString());
+    });
   }
 }

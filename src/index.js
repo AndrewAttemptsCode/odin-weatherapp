8437a2ecceadbd489fe24dcba1c1e1cf42f607e7
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-08-26 16:31:47 +0100

    Add weather app class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Testing 123');
+import WeatherApp from "./modules/weatherApp";
+
+const baseUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+const apiKey = "BEZ29GZQSBH53VG9GZQWMSF5H";
+
+const weatherApp = new WeatherApp(apiKey, baseUrl);
+
+weatherApp.showCurrentWeather("manchester");
---
+++ src/modules/weatherApp.js
@@ -0,0 +1,22 @@
+import WeatherAPI from "./weatherAPI";
+import WeatherData from "./weatherData";
+
+export default class WeatherApp {
+  constructor(apiKey, baseUrl) {
+    this.weatherAPI = new WeatherAPI(apiKey, baseUrl);
+  }
+
+  async showCurrentWeather(location) {
+    const weatherData = await this.weatherAPI.getCurrentWeather(location);
+    const currentWeather = new WeatherData(
+      weatherData.resolvedAddress,
+      weatherData.days[0].conditions,
+      weatherData.days[0].temp,
+      weatherData.days[0].tempmin,
+      weatherData.days[0].tempmax,
+      weatherData.days[0].humidity,
+      weatherData.days[0].feelslike
+    );
+    console.log(currentWeather);
+  }
+}

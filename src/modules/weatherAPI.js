4c72885289b64ef5ed8a508140deeafc3690529b
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-08-26 16:57:18 +0100

    Add method for retrieving weekly forecast and amend duration for current weather method to just the current day

--- src/modules/weatherAPI.js
+++ src/modules/weatherAPI.js
@@ -5,9 +5,15 @@ export default class WeatherAPI {
   }
 
   async getCurrentWeather(location) {
-    const response = await fetch(`${this.baseUrl}${location}?key=${this.apiKey}`);
+    const response = await fetch(`${this.baseUrl}${location}/today?key=${this.apiKey}`);
     const currentWeather = await response.json();
     return currentWeather;
+  }
+
+  async getWeeklyForecast(location) {
+    const response = await fetch(`${this.baseUrl}${location}/next7days?key=${this.apiKey}`);
+    const weeklyForecast = await response.json();
+    return weeklyForecast;
   }
 }
 

24905c85da580d47b91ed1445e8575cac05228b6
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-08-28 16:37:43 +0100

    Add DOM elements and assignment for the current weather display

--- src/modules/weatherData.js
+++ src/modules/weatherData.js
@@ -12,14 +12,65 @@ export default class WeatherData {
     this.feelsLike = feelsLike;
   }
 
-  currentToString() {
+  displayCurrent() {
     const formattedCurrentDate = formatCurrentDate(this.datetime);
     const formattedTemp = tempFormat(this.temp);
     const formattedMinTemp = tempFormat(this.tempMin);
     const formattedMaxTemp = tempFormat(this.tempMax);
     const formattedFeelsLike = tempFormat(this.feelsLike);
-    return `Date: ${formattedCurrentDate}, Temp: ${formattedTemp}, Location: ${this.location}, Condition: ${this.condition}, Temp Max: ${formattedMaxTemp}, Temp Min: ${formattedMinTemp}, Humidity: ${this.humidity}, Feels Like: ${formattedFeelsLike}`;
+
+    const currentSide = document.querySelector('.current-side');
+    // Location
+    const locationContainer = document.createElement('div');
+    const currentLocation = document.createElement('p');
+    currentLocation.classList.add('current-location');
+    currentLocation.textContent = `${this.location}`;
+    locationContainer.appendChild(currentLocation);
+    currentSide.appendChild(locationContainer);
+    // Date
+    const currentDate = document.createElement('p');
+    currentDate.classList.add('current-date');
+    currentDate.textContent = `${formattedCurrentDate}`;
+    locationContainer.appendChild(currentDate);
+    // Temp
+    const currentTemp = document.createElement('p');
+    currentTemp.classList.add('current-temp');
+    currentTemp.textContent = `${formattedTemp}`;
+    currentSide.appendChild(currentTemp);
+    // Condition
+    const currentCondition = document.createElement('p');
+    currentCondition.classList.add('current-condition');
+    currentCondition.textContent = `${this.condition}`;
+    currentSide.appendChild(currentCondition);
+    // Min/Max Container
+    const minMaxContainer = document.createElement('div');
+    minMaxContainer.classList.add('min-max-container');
+    currentSide.appendChild(minMaxContainer);
+    // Temp Max
+    const currentMax = document.createElement('p');
+    currentMax.classList.add('current-Max');
+    currentMax.textContent = `Max: ${formattedMaxTemp}`;
+    minMaxContainer.appendChild(currentMax);
+    // Temp Min
+    const currentMin = document.createElement('p');
+    currentMin.classList.add('current-min');
+    currentMin.textContent = `Min: ${formattedMinTemp}`;
+    minMaxContainer.appendChild(currentMin);
+    // Feels Like
+    const currentFeelsLike = document.createElement('p');
+    currentFeelsLike.classList.add('current-feels-like');
+    currentFeelsLike.textContent = `Feels Like: ${formattedFeelsLike}`;
+    currentSide.appendChild(currentFeelsLike);
   }
+
+  // currentToString() {
+  //   const formattedCurrentDate = formatCurrentDate(this.datetime);
+  //   const formattedTemp = tempFormat(this.temp);
+  //   const formattedMinTemp = tempFormat(this.tempMin);
+  //   const formattedMaxTemp = tempFormat(this.tempMax);
+  //   const formattedFeelsLike = tempFormat(this.feelsLike);
+  //   return `Date: ${formattedCurrentDate}, Temp: ${formattedTemp}, Location: ${this.location}, Condition: ${this.condition}, Temp Max: ${formattedMaxTemp}, Temp Min: ${formattedMinTemp}, Humidity: ${this.humidity}, Feels Like: ${formattedFeelsLike}`;
+  // }
 
   weeklyToString() {
     const formattedDate = formatWeeklyDate(this.datetime);

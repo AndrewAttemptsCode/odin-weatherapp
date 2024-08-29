cabae6a59b1a28c50bd47e160d91edb56f0ff280
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-08-29 15:42:46 +0100

    Add event listener to search form to handle search input

--- src/modules/getLocation.js
+++ src/modules/getLocation.js
@@ -1,6 +1,6 @@
 import searchImg from '../images/control-icons/search.svg';
 
-export default function getLocation() {
+export default function getLocation(weatherApp) {
   const header = document.querySelector('.header');
 
   const searchContainer = document.createElement('div');
@@ -15,15 +15,32 @@ export default function getLocation() {
   searchInput.type = 'text';
   searchInput.id = 'location';
   searchInput.classList.add('search-input');
+  searchInput.autocomplete = 'off';
   searchInput.placeholder = 'Search Location';
   searchForm.appendChild(searchInput);
 
   const searchButton = document.createElement('button');
   searchButton.type = 'submit';
+  searchButton.title = 'Search location';
   searchButton.classList.add('search-button');
   searchForm.appendChild(searchButton);
 
   const searchImage = document.createElement('img');
   searchImage.src = searchImg;
   searchButton.appendChild(searchImage);
+
+  searchForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    let location = searchInput.value.trim();
+
+    if (location === '') {
+      location = 'manchester';
+    }
+
+    await weatherApp.showCurrentWeather(location);
+    await weatherApp.showWeeklyForecast(location);
+    await weatherApp.showHourlyForecast(location);
+    searchInput.value = '';
+  });
 }

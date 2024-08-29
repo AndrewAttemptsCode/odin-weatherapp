43b6a972671df54fa11fcc647ab898189f836453
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-08-29 19:17:50 +0100

    Add function to set weather icon

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,3 +1,26 @@
+import clearDayIcon from '../images/weather-icons/clear-day.svg';
+import rainIcon from '../images/weather-icons/rain.svg';
+import clearNightIcon from '../images/weather-icons/clear-night.svg';
+import cloudyIcon from '../images/weather-icons/cloudy.svg';
+import partyCloudyDayIcon from '../images/weather-icons/partly-cloudy-day.svg';
+import partlyCloudyNightIcon from '../images/weather-icons/partly-cloudy-night.svg';
+import rainSnowShowersNightIcon from '../images/weather-icons/rain-snow-showers-night.svg';
+import showersDayIcon from '../images/weather-icons/showers-day.svg';
+import showersNightIcon from '../images/weather-icons/showers-night.svg';
+import snowShowersDayIcon from '../images/weather-icons/snow-showers-day.svg';
+import snowShowersNightIcon from '../images/weather-icons/snow-showers-night.svg';
+import thunderIcon from '../images/weather-icons/thunder.svg';
+import thunderShowersNightIcon from '../images/weather-icons/thunder-showers-night.svg';
+import windIcon from '../images/weather-icons/wind.svg';
+import fogIcon from '../images/weather-icons/fog.svg';
+import hailIcon from '../images/weather-icons/hail.svg';
+import rainSnowIcon from '../images/weather-icons/rain-snow.svg';
+import rainSnowShowersIcon from '../images/weather-icons/rain-snow-showers-day.svg';
+import sleetIcon from '../images/weather-icons/sleet.svg';
+import snowIcon from '../images/weather-icons/snow.svg';
+import thunderRainIcon from '../images/weather-icons/thunder-rain.svg';
+import thunderShowersDayIcon from '../images/weather-icons/thunder-showers-day.svg';
+
 export function formatTime(timeString) {
   const [hours, minutes] = timeString.split(':');
   const hoursNumber = Number(hours);
@@ -33,4 +56,35 @@ export function tempFormat(tempValue) {
   const tempC = ((tempNumber - 32) * 5) / 9;
   const tempFormatted = tempC.toFixed(0);
   return `${tempFormatted}°C`;
+}
+
+const weatherIconSet = {
+  clear-day: clearDayIcon,
+  rain: rainIcon,
+  clear-night: clearNightIcon,
+  cloudy: cloudyIcon,
+  partly-cloudy-day: partyCloudyDayIcon,
+  rain-snow-showers-night: rainSnowShowersNightIcon,
+  snow-showers-day: snowShowersDayIcon,
+  showers-day: showersDayIcon,
+  snow-showers-night: snowShowersNightIcon,
+  wind: windIcon,
+  showers-night: showersNightIcon,
+  thunder: thunderIcon,
+  fog: fogIcon,
+  rain-snow: rainSnowIcon,
+  sleet: sleetIcon,
+  thunder-rain: thunderRainIcon,
+  hail: hailIcon,
+  rain-snow-showers-day: rainSnowShowersIcon,
+  snow: snowIcon,
+  thunder-showers-day: thunderShowersDayIcon,
+  thunder-showers-night: thunderShowersNightIcon,
+  partly-cloudy-night: partlyCloudyNightIcon
+};
+
+export function setWeatherIcon(icon) {
+  const weatherBG = document.querySelector('.main');
+  const image = weatherIconSet[icon];
+  weatherBG.style.backgroundImage = `url(${image})`;
 }
